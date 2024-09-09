e9fe9cf02820d370426a1bf34015612fb4e47f40
Igor <igorspinoladev@gmail.com>
2024-09-09 17:18:46 -0300

    calculate empty size method added

--- src/Recinto.js
+++ src/Recinto.js
@@ -8,6 +8,17 @@ class Recinto {
     nome() {
         return `Recinto ${this.numero}`;
     }
+    tamanhoLivre(animal, qtd) {
+        const tamanhos = {
+            "LEAO": 3,
+            "LEOPARDO": 2,
+            "CROCODILO": 3,
+            "MACACO": 1, 
+            "GAZELA": 2,
+            "HIPOPOTAMO": 4,
+        }
+        return this.tamanho - (tamanhos[animal] * qtd);
+    }
 }
 
 const recintos = [];
